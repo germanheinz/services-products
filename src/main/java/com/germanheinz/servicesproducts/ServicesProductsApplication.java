b8e8adb3247774ebb2cf1d92133e6f4deceec6bd
germanheinz <heinz.german@gmail.com>
2020-10-05 13:36:49 +0200

    Initial commit

---
+++ src/main/java/com/germanheinz/servicesproducts/ServicesProductsApplication.java
@@ -0,0 +1,13 @@
+package com.germanheinz.servicesproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicesProductsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/germanheinz/servicesproducts/ServicesProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.germanheinz.servicesproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
