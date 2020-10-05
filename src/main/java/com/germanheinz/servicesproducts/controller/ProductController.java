527b6aca6bfc270639ac7f3e78ad914347d1478f
germanheinz <heinz.german@gmail.com>
2020-10-05 15:49:32 +0200

    Products - Controller and Services

---
+++ src/main/java/com/germanheinz/servicesproducts/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.germanheinz.servicesproducts.controller;
+
+import com.germanheinz.servicesproducts.entity.Product;
+import com.germanheinz.servicesproducts.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    private ProductService productService;
+
+    @GetMapping("/findAll")
+    public List<Product> getAllProducts(){
+        return productService.findAll();
+    }
+
+    @GetMapping("/find/{id}")
+    public Product getById(@PathVariable("id") Long id){
+        return productService.findById(id);
+    }
+
+}
