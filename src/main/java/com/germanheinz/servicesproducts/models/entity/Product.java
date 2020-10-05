a4c18f8e8a21f99935d8e1fa112aaebcbc7b0a3f
germanheinz <heinz.german@gmail.com>
2020-10-05 14:29:57 +0200

    Model Created, This closes #1

---
+++ src/main/java/com/germanheinz/servicesproducts/models/entity/Product.java
@@ -0,0 +1,30 @@
+package com.germanheinz.servicesproducts.models.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name= "products")
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = -6452787960092603450L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double price;
+
+    @Column(name = "created_date")
+    @Temporal(TemporalType.DATE)
+    private Date createdDate;
+
+
+
+}
