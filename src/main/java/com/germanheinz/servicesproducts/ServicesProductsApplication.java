16b2410eab5e5da35c3ad14eb4658896902e3b84
germanheinz <heinz.german@gmail.com>
2020-10-07 18:08:59 +0200

    Eureka Server - Config

--- src/main/java/com/germanheinz/servicesproducts/ServicesProductsApplication.java
+++ src/main/java/com/germanheinz/servicesproducts/ServicesProductsApplication.java
@@ -2,7 +2,9 @@ package com.germanheinz.servicesproducts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
+@RibbonClient(name = "services-products")
 @SpringBootApplication
 public class ServicesProductsApplication {
 
